a27e31fa0eccbc9ac1f8caa640b997eceecae3a3
herozxb <herozxb@gmail.com>
2019-10-22 17:56:54 +0800

    doc model predict

--- Server/Django/forFlutter/forIOS2/settings.py
+++ Server/Django/forFlutter/forIOS2/settings.py
@@ -20,10 +20,33 @@ import os
 
 from docproduct.predictor import RetreiveQADoc
 
-pretrained_path = 'BioBertFolder/biobert_v1.0_pubmed_pmc/'
+pretrained_path = '/home/deep/bio-bert/BioBertFolder/biobert_v1.0_pubmed_pmc/'
 # ffn_weight_file = None
-bert_ffn_weight_file = 'newFolder/models/bertffn_crossentropy/bertffn'
-embedding_file = 'Float16EmbeddingsExpanded5-27-19.pkl'
+bert_ffn_weight_file = '/home/deep/bio-bert/newFolder/models/bertffn_crossentropy/bertffn'
+embedding_file = '/home/deep/bio-bert/Float16EmbeddingsExpanded5-27-19.pkl'
+
+doc = RetreiveQADoc(pretrained_path=pretrained_path,
+ffn_weight_file=None,
+bert_ffn_weight_file=bert_ffn_weight_file,
+embedding_file=embedding_file)
+
+question_text = "Girl's arm, hair is long and long"
+
+search_similarity_by = 'answer'  #@param ['answer', "question"]
+
+number_results_toReturn=10 #@param {type:"number"}
+
+answer_only=True #@param ["False", "True"] {type:"raw"}
+
+returned_results = doc.predict( question_text ,
+                  search_by=search_similarity_by, topk=number_results_toReturn, answer_only=answer_only)
+
+print('')
+for jk in range(len(returned_results)):
+    print("Result ", jk+1)
+    print(returned_results[jk])
+    print('')
+
 
 #import mysql.connector
 
